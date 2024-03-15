41d2264484aedc5584012160d9099b9526cfcccd
husnasaleah <119001414+husnasaleah@users.noreply.github.com>
2024-03-15 10:42:39 +0700

    not finished yet

--- FR_flask/app6.py
+++ FR_flask/app6.py
@@ -31,7 +31,7 @@ mycursor = mydb.cursor()
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
-def insert_face(emp_id, emotion, age, gender, imgS_blob, imgL_blob):
+def insert_face(img_id, emotion, age, gender, imgS_blob, imgL_blob):
 
     # Convert the image to a binary format
     _, imgS_encoded = cv2.imencode('.jpg', imgS_blob)
@@ -41,8 +41,19 @@ def insert_face(emp_id, emotion, age, gender, imgS_blob, imgL_blob):
     imgL_blob = imgL_encoded.tobytes()
 
     try:
+        mycursor.execute("SELECT a.img_person, b.emp_name "
+                        "FROM img_dataset a "
+                        "LEFT JOIN employee b ON a.img_person = b.emp_id "
+                        "WHERE img_id = %s", (img_id,))
+        row = mycursor.fetchone()
+        if row:
+            emp_id, emp_name = row
+        else:
+            emp_id = -1
+            emp_name = "unknown"
+        
         mycursor.execute("INSERT INTO detection (det_date, det_person, det_img_face, det_img_env, det_emo, det_age, det_gender) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                        (str(date.today()), emp_id, imgS_blob, imgL_blob, emotion, age, gender, ))
+                        (str(date.today()), emp_id, imgS_blob, imgL_blob, emotion, age, gender))
         mydb.commit()
         print("Image path saved in the database.")
     except Exception as e:
@@ -50,32 +61,25 @@ def insert_face(emp_id, emotion, age, gender, imgS_blob, imgL_blob):
         print("Error executing INSERT:", e)
 
 
-def analyze_face(face_roi, x, y, w, h, img_flipped, saved_faces):
+
+
+def analyze_face(face_roi, x, y, w, h, cam_flip, saved_faces):
     try:
         analysis = DeepFace.analyze(face_roi, actions=['emotion', 'age', 'gender'], enforce_detection=False)
         emotion = analysis[0]['dominant_emotion']
         age = analysis[0]['age']
         gender = analysis[0]['dominant_gender']
         print(emotion,age,gender)
-        result = DeepFace.find(face_roi, db_path="dataset/", enforce_detection=False)
+        result = DeepFace.find(face_roi, db_path="dataset/", enforce_detection=False, model_name="VGG-Face")
         if len(result[0]['identity']) > 0:
             img_id = result[0]['identity'][0].split('/')[-1].split('.')[0]
-            try:
-                mycursor.execute("select a.img_person "
-                            "  from img_dataset a "
-                            "  left join employee b on a.img_person = b.emp_id "
-                            " where img_id = " + img_id)
-                
-                
-                row = mycursor.fetchone()
-                emp_id = row[0]
-            except Exception as e:
-                print("Error executing SQL query or fetching data:", e)
+            print("img_id"+img_id)
+    
         else:
             img_id = -1
         
                
-        insert_face(emp_id, emotion, age, gender, face_roi, img_flipped)
+        insert_face(img_id, emotion, age, gender, face_roi, cam_flip)
 
         face_id = f"{x}-{y}-{w}-{h}"
         saved_faces.add(face_id)
@@ -86,6 +90,7 @@ def analyze_face(face_roi, x, y, w, h, img_flipped, saved_faces):
 def gen_frames():
     trackers = []  
     saved_faces = set()  
+
     wCam, hCam = 400, 400
     cap = cv2.VideoCapture(0)
     cap.set(3, wCam)
@@ -97,32 +102,31 @@ def gen_frames():
         if not success:
             break
 
-        img_resized = cv2.resize(img, (640, 480))
-        img_flipped = cv2.flip(img_resized, 1)
+        cam_resize = cv2.resize(img, (640, 480))
+        cam_flip = cv2.flip(cam_resize, 1)
 
        
-        trackers = [tracker for tracker in trackers if tracker.update(img_flipped)[0]]
+        trackers = [tracker for tracker in trackers if tracker.update(cam_flip)[0]]
 
-        gray_scale = cv2.cvtColor(img_flipped, cv2.COLOR_BGR2GRAY)
+        gray_scale = cv2.cvtColor(cam_flip, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray_scale, 1.1, 4)
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(img_flipped, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.rectangle(cam_flip, (x, y), (x+w, y+h), (255, 255, 0), 2)
 
        
         
         if not trackers:
             for (x, y, w, h) in faces:
-                face_roi = img_flipped[y:y+h, x:x+w]
-                threading.Thread(target=analyze_face, args=(face_roi, x, y, w, h, img_flipped, saved_faces)).start()
-                tracker = cv2.legacy.TrackerKCF_create()
-
-                tracker.init(img_flipped, (x, y, w, h))
+                face_roi = cam_flip[y:y+h, x:x+w]
+                threading.Thread(target=analyze_face, args=(face_roi, x, y, w, h, cam_flip, saved_faces)).start()
+                tracker = cv2.TrackerKCF_create()
+                tracker.init(cam_flip, (x, y, w, h))
                 trackers.append(tracker)
 
 
     
-        ret, buffer = cv2.imencode('.jpg', img_flipped)
+        ret, buffer = cv2.imencode('.jpg', cam_flip)
         frame = buffer.tobytes()
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -157,11 +161,12 @@ def countTodayScan():
 
 @app.route('/loadData', methods=['GET', 'POST'])
 def load_data():
-    mycursor.execute("SELECT a.det_person, a.det_img_face, IFNULL(b.emp_name, 'unknown') AS emp_name, a.det_emo, a.det_age, a.det_gender "
-                     "FROM detection a "
-                     "LEFT JOIN employee b ON a.det_person = b.emp_id "
-                     "WHERE a.det_date = CURDATE() "
-                     "ORDER BY a.det_added DESC")
+    mycursor.execute(" SELECT a.det_person, a.det_img_face, IF(a.det_person = -1, 'unknown', IFNULL(b.emp_name, 'unknown')) AS emp_name, a.det_emo, a.det_age, a.det_gender "
+                    " FROM detection a "
+                    " LEFT JOIN employee b ON a.det_person = b.emp_id "
+                    " WHERE a.det_date = CURDATE() "
+                    " ORDER BY a.det_added DESC "
+                    )
     data = mycursor.fetchall()
 
     result = []
