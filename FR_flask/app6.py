9eb29888e101dbd4fd61a9a7f2a26f5c06021ce1
husnasaleah <119001414+husnasaleah@users.noreply.github.com>
2024-03-18 11:47:45 +0700

    add sound and delete file

--- FR_flask/app6.py
+++ FR_flask/app6.py
@@ -10,7 +10,7 @@ import numpy as np
 import base64
 import mysql.connector
 import threading
-from concurrent.futures import ThreadPoolExecutor  # เพิ่มเข้ามา
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 app = Flask(__name__)
@@ -31,7 +31,7 @@ mycursor = mydb.cursor()
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
-def insert_face(img_id, emotion, age, gender, imgS_blob, imgL_blob):
+def insert_face(emp_id, emotion, age, gender, imgS_blob, imgL_blob):
 
     # Convert the image to a binary format
     _, imgS_encoded = cv2.imencode('.jpg', imgS_blob)
@@ -41,56 +41,52 @@ def insert_face(img_id, emotion, age, gender, imgS_blob, imgL_blob):
     imgL_blob = imgL_encoded.tobytes()
 
     try:
-        mycursor.execute("SELECT a.img_person, b.emp_name "
-                        "FROM img_dataset a "
-                        "LEFT JOIN employee b ON a.img_person = b.emp_id "
-                        "WHERE img_id = %s", (img_id,))
-        row = mycursor.fetchone()
-        if row:
-            emp_id, emp_name = row
-        else:
-            emp_id = -1
-            emp_name = "unknown"
-        
         mycursor.execute("INSERT INTO detection (det_date, det_person, det_img_face, det_img_env, det_emo, det_age, det_gender) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                        (str(date.today()), emp_id, imgS_blob, imgL_blob, emotion, age, gender))
+                        (str(date.today()), emp_id, imgS_blob, imgL_blob, emotion, age, gender, ))
         mydb.commit()
         print("Image path saved in the database.")
     except Exception as e:
         mydb.rollback()
         print("Error executing INSERT:", e)
 
-
-
-
-def analyze_face(face_roi, x, y, w, h, cam_flip, saved_faces):
+executor = ThreadPoolExecutor(max_workers=2)
+def analyze_face(face_roi, x, y, w, h, img_flipped, saved_faces):
     try:
         analysis = DeepFace.analyze(face_roi, actions=['emotion', 'age', 'gender'], enforce_detection=False)
         emotion = analysis[0]['dominant_emotion']
         age = analysis[0]['age']
         gender = analysis[0]['dominant_gender']
-        print(emotion,age,gender)
-        result = DeepFace.find(face_roi, db_path="dataset/", enforce_detection=False, model_name="VGG-Face")
+
+        result = DeepFace.find(face_roi, db_path="dataset/", enforce_detection=False)
         if len(result[0]['identity']) > 0:
-            img_id = result[0]['identity'][0].split('/')[-1].split('.')[0]
-            print("img_id"+img_id)
-    
+            img_id = result['identity'][0].split('/')[-1].split('.')[0]
+            emp_id = None  # Initialize emp_id
+            try:
+                mycursor.execute("SELECT img_person FROM img_dataset WHERE img_id = %s", (img_id,))
+                row = mycursor.fetchone()
+                if row:
+                    emp_id = row[0]
+                else:
+                    emp_id = -1  # Unknown person
+            except Exception as e:
+                print("Error executing SQL query or fetching data:", e)
+                emp_id = -1  # Fallback in case of query error
         else:
-            img_id = -1
-        
+            emp_id = -1  # Unknown person
                
-        insert_face(img_id, emotion, age, gender, face_roi, cam_flip)
+        insert_face(emp_id, emotion, age, gender, face_roi, img_flipped)
 
         face_id = f"{x}-{y}-{w}-{h}"
         saved_faces.add(face_id)
     except Exception as e:
         print("Error in processing:", e)
-        
+
 
 def gen_frames():
+    global executor
     trackers = []  
     saved_faces = set()  
-
+    futures = []
     wCam, hCam = 400, 400
     cap = cv2.VideoCapture(0)
     cap.set(3, wCam)
@@ -102,31 +98,41 @@ def gen_frames():
         if not success:
             break
 
-        cam_resize = cv2.resize(img, (640, 480))
-        cam_flip = cv2.flip(cam_resize, 1)
+        img_resized = cv2.resize(img, (640, 480))
+        img_flipped = cv2.flip(img_resized, 1)
 
        
-        trackers = [tracker for tracker in trackers if tracker.update(cam_flip)[0]]
+        trackers = [tracker for tracker in trackers if tracker.update(img_flipped)[0]]
 
-        gray_scale = cv2.cvtColor(cam_flip, cv2.COLOR_BGR2GRAY)
+        gray_scale = cv2.cvtColor(img_flipped, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray_scale, 1.1, 4)
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(cam_flip, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            cv2.rectangle(img_flipped, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
        
         
         if not trackers:
             for (x, y, w, h) in faces:
-                face_roi = cam_flip[y:y+h, x:x+w]
-                threading.Thread(target=analyze_face, args=(face_roi, x, y, w, h, cam_flip, saved_faces)).start()
-                tracker = cv2.TrackerKCF_create()
-                tracker.init(cam_flip, (x, y, w, h))
+                face_roi = img_flipped[y:y+h, x:x+w]
+                # Submit tasks to the executor instead of starting new threads directly
+                future = executor.submit(analyze_face, face_roi, x, y, w, h, img_flipped, saved_faces)
+                futures.append(future)
+                tracker = cv2.legacy.TrackerKCF_create()
+                tracker.init(img_flipped, (x, y, w, h))
                 trackers.append(tracker)
+
+        # Optionally, handle completed futures if you need to process results
+        for future in as_completed(futures):
+            try:
+                # Result handling if necessary...
+                future.result()
+            except Exception as exc:
+                print(f'Generated an exception: {exc}')
 
 
     
-        ret, buffer = cv2.imencode('.jpg', cam_flip)
+        ret, buffer = cv2.imencode('.jpg', img_flipped)
         frame = buffer.tobytes()
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -148,25 +154,32 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/countTodayScan')
+@app.route('/countTodayScan', methods=['GET'])
 def countTodayScan():
-    mycursor.execute("select count(*) "
-                     "  from detection "
-                     " where det_date = curdate() ")
-    row = mycursor.fetchone()
-    rowcount = row[0]
- 
-    return jsonify({'rowcount': rowcount})
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="flask_db"
+    )
+    mycursor = mydb.cursor()
+    try:
+        mycursor.execute("SELECT COUNT(*) FROM detection WHERE det_date = CURDATE()")
+        rowcount = mycursor.fetchone()[0]
+        return jsonify({'rowcount': rowcount})
+    except Exception as e:
+        print(f"Error in countTodayScan: {e}")
+        return jsonify({'error': str(e)}), 500
+
  
 
 @app.route('/loadData', methods=['GET', 'POST'])
 def load_data():
-    mycursor.execute(" SELECT a.det_person, a.det_img_face, IF(a.det_person = -1, 'unknown', IFNULL(b.emp_name, 'unknown')) AS emp_name, a.det_emo, a.det_age, a.det_gender "
-                    " FROM detection a "
-                    " LEFT JOIN employee b ON a.det_person = b.emp_id "
-                    " WHERE a.det_date = CURDATE() "
-                    " ORDER BY a.det_added DESC "
-                    )
+    mycursor.execute("SELECT a.det_person, a.det_img_face, IFNULL(b.emp_name, 'unknown') AS emp_name, a.det_emo, a.det_age, a.det_gender "
+                     "FROM detection a "
+                     "LEFT JOIN employee b ON a.det_person = b.emp_id "
+                     "WHERE a.det_date = CURDATE() "
+                     "ORDER BY a.det_added DESC")
     data = mycursor.fetchall()
 
     result = []
@@ -181,4 +194,8 @@ def load_data():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    try:
+        app.run(host='127.0.0.1', port=5000, debug=True)
+    finally:
+        executor.shutdown(wait=True)  # Properly shutdown the executor when the app closes
+        
